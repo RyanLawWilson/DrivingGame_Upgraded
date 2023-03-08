88843d602d084d6ee6e17c587608c2a3bd2f9d21
Ryan Wilson <ryan.law.wilson@gmail.com>
2023-03-08 16:16:32 -0700

    Create spawn locations for spawning in packages

---
+++ Scripts/PackageSpawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PackageSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject spawnLocationsContainer;
+    [SerializeField] GameObject packagesContainer;
+    [SerializeField] List<Package> listOfSpawnablePackages;
+    Transform[] packageSpawns;
+
+    private void Awake() {
+        packageSpawns = spawnLocationsContainer.GetComponentsInChildren<Transform>();
+    }
+
+    private void Start() {
+        SpawnPackages(10);
+    }
+
+    public void SpawnPackage(int spawnPoint = -1) {
+        if (spawnPoint == -1) {spawnPoint = Random.Range(0, packageSpawns.Length);}
+
+
+    }
+
+    public void SpawnPackages(int quantity) {
+        for (int i = 0; i < quantity; i++) {
+            Instantiate(
+                listOfSpawnablePackages[Random.Range(0, listOfSpawnablePackages.Count)],
+                packageSpawns[Random.Range(0, Random.Range(0, packageSpawns.Length))].position,
+                Quaternion.identity,
+                packagesContainer.transform);
+        }
+    }
+
+}
