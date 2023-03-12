bc59afc4ad968d3d7146534a5e030ac2d05b4417
Ryan Wilson <ryan.law.wilson@gmail.com>
2023-03-11 20:19:26 -0700

    Implement floating text to show player what customer excepts what packages.

--- Scripts/Customer.cs
+++ Scripts/Customer.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Customer : MonoBehaviour
 {
     [SerializeField] string customerName;
     [SerializeField] List<int> acceptablePackageSizes;
 
+    // This is to show the correct sprite according to the sprite renderer that I created.
+    
     public List<int> GetAcceptablePackageSizesList() {
         return acceptablePackageSizes;
     }
---
+++ Scripts/PlayerDetectionRadius.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerDetectionRadius : MonoBehaviour
+{
+    Dictionary<int, int> packageSizeToSpriteIdDictionry;
+
+    [SerializeField] TextMeshPro floatingText;
+    private Customer customer;
+
+    private void Awake() {
+        customer = gameObject.GetComponentInParent<Customer>();
+        
+        packageSizeToSpriteIdDictionry = new Dictionary<int, int>() {
+            {1,0}, {2,3}, {3,2}, {5,1}, {7,4}
+        };
+
+        List<int> acceptablePackages = customer.GetAcceptablePackageSizesList();
+
+        floatingText.text = string.Empty;
+        foreach (int size in acceptablePackages) {
+            floatingText.text += $"<sprite={packageSizeToSpriteIdDictionry[size]}> ";
+        }
+
+        floatingText.text = floatingText.text.TrimEnd();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            floatingText.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Player") {
+            floatingText.gameObject.SetActive(false);
+        }
+    }
+}
